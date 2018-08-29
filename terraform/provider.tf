e8e62eaa021e7bd81884477a23e6028ab23dfbab
jx_git <skaarj.sergey@gmail.com>
2018-08-29 17:30:09 +0300

    readme, terraform dependencies

--- terraform/provider.tf
+++ terraform/provider.tf
@@ -6,8 +6,6 @@ provider "google" {
 data "google_compute_zones" "available" {
   depends_on = ["google_project_services.project"]
 }
-#data "google_client_config" "current" {}
 data "google_project" "current" {
     depends_on = ["google_project_services.project"]
 }
-#data "google_project_services" "current" {}
